444ad6e4b7870cacc579e064c5c896976a08b172
DavaRava <david-lasar@web.de>
2026-03-06 19:10:57 +0100

    config update

--- src/main/java/de/davarava/extrahammers/Config.java
+++ src/main/java/de/davarava/extrahammers/Config.java
@@ -7,8 +7,12 @@ public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.BooleanValue IS_BLOCK_OUTLINE_BLACK = BUILDER
-            .comment("Whether the outline color on a block is black when holding a hammer. If not than the color is white.")
-            .define("hammer_block_outline_black", true);
+            .comment("Whether the outline color on a block is black when holding a hammer. If not than the default color white will be used.")
+            .define("hammer_block_outline_black", false);
+
+    public static final ModConfigSpec.BooleanValue PARTICLES = BUILDER
+            .comment("Whether particles will be shown after breaking blocks with a hammer.")
+            .define("hammer_particles", true);
 
     static final ModConfigSpec SPEC = BUILDER.build();
 }
